8923a4af99ea105666013438d2af4266b590f855
Jim Publicover <jameslpublicover@gmail.com>
2016-10-07 16:35:12 -0400

    reservation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+
+  get 'reservations/show'
+
+  get 'reservations/new'
+
+  get 'reservations/edit'
+
+  get 'reservations/create'
+
+  get 'reservations/update'
+
+  get 'reservations/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
